1c632f3e68bf0587394c43121e2a205f931798e3
Jocelyn & Chris <jocelynchris@learnacademy.org>
2017-01-09 15:10:20 -0800

    story 2 completed, added map showing marker to individual listing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :apartments
+  resources :apartments do
+    get 'map_location' => 'apartments#map_location'
+  end
   root 'apartments#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -11,4 +11,28 @@ RSpec.feature "UserStories", type: :feature do
       end
     end
   end
+
+  context 'Adding a new apartment' do
+    Steps 'Creating a new listing' do
+      Given 'I am on the New Apartment page' do
+        visit '/apartments/new'
+      end
+      Then 'I fill in the given fields' do
+        fill_in 'apartment[street1]', with: '1 Arcadia Street'
+        fill_in 'apartment[street2]', with: 'Apt 7'
+        fill_in 'apartment[city]', with: 'Boston'
+        fill_in 'apartment[state]', with: 'MA'
+        fill_in 'apartment[country]', with: 'USA'
+        fill_in 'apartment[name]', with: 'Paul Bunyan'
+        fill_in 'apartment[phone]', with: '4'
+        fill_in 'apartment[hours]', with: 'Every'
+      end
+      And 'I can submit the form' do
+        click_button 'Create Apartment'
+      end
+      Then 'I am taken to the show listing page and can see the new listing' do
+        expect(page).to have_content("Street1: 1 Arcadia Street")
+      end
+    end
+  end
 end
